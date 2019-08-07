55c9a462e7d7b01e7d5a760c6cd20863b6735cf0
rhyamada <rhyamada@gmail.com>
2019-08-07 17:04:31 -0300

    added code

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+from ebaysdk.finding import Connection as Finding
+from ebaysdk.exception import ConnectionError
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'OK!'
-
+    try:
+        api = Finding(appid=os.environ['EBAY'], config_file=None)
+        api_request = {
+            'itemFilter': [
+                {'name': 'ListingType','value': 'Auction'},
+                {'name': 'Seller','value': os.environ['SELLERS'].split(' ')},
+                {'name': 'MaxPrice', 'value': '15', 'paramName': 'Currency', 'paramValue': 'USD'},
+            ],
+            'sortOrder': 'EndTimeSoonest',
+            'paginationInput': {
+                'entriesPerPage': '10',
+                'pageNumber': '1' 	 
+            },        
+        }    
+        response = api.execute('findItemsAdvanced', api_request)
+        return jsonify(response.dict())
+    except ConnectionError as e:
+        return str(e)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=os.environ['PORT'])
