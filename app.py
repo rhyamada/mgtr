e25b8c93b0763da426be7f16300e507e41d31ee5
rhyamada <rhyamada@gmail.com>
2019-08-08 16:49:55 -0300

    bootstrap

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
-from flask import Flask, jsonify, request
-from ebaysdk.finding import Connection as Finding
-from ebaysdk.exception import ConnectionError
-import os,json,re
-from datetime import datetime
-from datetime import timedelta
+import os,json,re,traceback
+from flask import Flask, jsonify, request, render_template
+from ebaysdk.finding import Connection
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 ire = re.compile('P(?P<days>[0-9]+)DT(?P<hours>[0-9]+)H(?P<minutes>[0-9]+)M(?P<seconds>[0-9]+)S')
 
 @app.route('/')
 def index():
-    price = float(request.values.get('p',10))
-    hours = float(request.values.get('h',8))
+    error = "error"
     try:
-        d = datetime.now()+timedelta(seconds=3600*hours)
-        api = Finding(appid=os.environ['EBAY'], config_file=None)
-        api_request = {
-            'itemFilter': [
+        price = float(request.values.get('p',10))
+        diff = timedelta(hours=float(request.values.get('h',6)))
+        datetime.now()+diff
+        d = datetime.now()+diff
+        items = []
+        api_request = dict(
+            itemFilter=[
                 {'name': 'Currency', 'value':'USD'},
-                {'name': 'ListingType','value': 'Auction'},
-                {'name': 'Seller','value': os.environ['SELLERS'].split(' ')},
-                {'name': 'MaxPrice', 'value': price, 'paramName': 'Currency', 'paramValue': 'USD'},
+                {'name': 'ListingType', 'value': 'Auction'},
+                {'name': 'Seller', 'value': os.environ['SELLERS'].split(' ')},
+                {'name': 'MaxPrice', 'value': price },
                 {'name': 'EndTimeTo',   'value': d.isoformat()}
             ],
-            'sortOrder': 'EndTimeSoonest',
-        }
-        res = api.execute('findItemsAdvanced', api_request).dict()
-        html = '' 
+            paginationInput=dict(entriesPerPage=20),
+
+            sortOrder='EndTimeSoonest'
+        )
+        res = Connection(appid=os.environ['EBAY'], config_file=None).execute('findItemsAdvanced', api_request).dict()
         if 'item' in res['searchResult']:
             for i in res['searchResult']['item']:
                 i['json']=json.dumps(i,indent=4)
@@ -35,23 +36,12 @@ def index():
                     continue
                 m = ire.match(i['sellingStatus']['timeLeft'])
                 if m:
-                    i['sellingStatus']['timeLeft'] = str(timedelta(**{k:int(v) for k,v in m.groupdict().items()}))
-                html = html + '''
-                
-                <details>
-                    <summary>
-                    <code style="white-space:pre;"><span style="display:inline-block;width:16px;"><img style="height:1em;" src="{galleryURL}"/></span>[{sellingStatus[timeLeft]}] {price:>5.2f}$ - {title}</code>
-                    </summary>
-                    <img src="{galleryURL}"/>
-                    <details>
-                        <summary>json</summary>
-                        <pre>{json}</pre>
-                    </details>            
-                </details>
-                '''.format(**i)
-        return html    
+                    i['diff'] = timedelta(**{k:int(v) for k,v in m.groupdict().items()})
+                items.append(i)
+        error = None
     except Exception as e:
-        return str(locals())
+        error = traceback.format_exc()
+    return render_template('index.html',**locals())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=os.environ['PORT'])
